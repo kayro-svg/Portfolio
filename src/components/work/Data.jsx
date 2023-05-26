db58dd93fddc139717b079b8dc77dc119f3d8557
Kayro Valero <128401413+kayro-svg@users.noreply.github.com>
2023-05-26 13:06:44 -0500

    Portfolio Update

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -2,33 +2,41 @@ import Work1 from "../../assets/work1AutoParts.jpg"
 import Work2 from "../../assets/work2Mydeplus.jpg"
 import Work3 from "../../assets/work3Walcakes.jpg"
 import Work4 from "../../assets/work4Fotoformas.jpg"
+import Work5 from "../../assets/work5Roxxet.jpg"
 
 export const projectsData = [
     { 
         id: 1,
         image: Work1,
-        title: "Website eCommerce Development",
+        title: "Website eCommerce - AutoPartes",
         category: 'web',
         url: "https://zonasurautopartes.com/store/",
     },
     {
         id: 2,
         image: Work2,
-        title: "Website eCommerce Redesign",
+        title: "Website Redesign - MydePlus",
         category: 'design',
         url: "https://mydeplus.net/",
     },
     {
         id: 3,
+        image: Work5,
+        title: "Website eCommerce - Roxxet",
+        category: 'web',
+        url: "https://roxett.com.mx/",
+    },
+    {
+        id: 4,
         image: Work3,
-        title: "Website Landing Page Development",
+        title: "Website Landing Page - Walcakes",
         category: 'web',
         url: "https://kayro-svg.github.io/Landing-Page-JS-PHP-HTML-CSS/",
     },
     {
-        id: 4,
+        id: 5,
         image: Work4,
-        title: "Website eCommerce Design",
+        title: "Website Design - FotoFormas",
         category: 'design',
         url: "https://foto-formas.webflow.io/",
     },
