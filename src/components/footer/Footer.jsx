fba0d9caba7942173c1691df4af092181846582e
Kayro Valero <sannelvalero@gmail.com>
2025-01-17 16:32:15 -0500

    Add new Babel plugin and refactor component code for consistency

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,44 +1,67 @@
-import React from 'react'
-import "./footer.css"
+import React from "react";
+import "./footer.css";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className="footer_container container">
-                <h1 className="footer_title">VALERO</h1>
+  return (
+    <footer className="footer">
+      <div className="footer_container container">
+        <h1 className="footer_title">VALERO</h1>
 
-                <ul className="footer_list">
-                    <li>
-                        <a href="#about" className="footer_link">About</a>
-                    </li>
+        <ul className="footer_list">
+          <li>
+            <a href="#about" className="footer_link">
+              About
+            </a>
+          </li>
 
-                    <li>
-                        <a href="#portfolio" className="footer_link">Projects</a>
-                    </li>
+          <li>
+            <a href="#portfolio" className="footer_link">
+              Projects
+            </a>
+          </li>
 
-                    <li>
-                        <a href="#testimonials" className="footer_link">Testimonials</a>
-                    </li>
-                </ul>
+          <li>
+            <a href="#testimonials" className="footer_link">
+              Testimonials
+            </a>
+          </li>
+        </ul>
 
-                <div className="footer_social">
-                    <a href="https://www.linkedin.com/in/kayro-valero/" className="footer_social-link" target="_blank">
-                        <i class="uil uil-linkedin-alt"></i>
-                    </a>
+        <div className="footer_social">
+          <a
+            href="https://www.linkedin.com/in/kayro-valero/"
+            className="footer_social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="uil uil-linkedin-alt"></i>
+          </a>
 
-                    <a href="https://github.com/kayro-svg" className="footer_social-link" target="_blank">
-                        <i class="uil uil-github-alt"></i>
-                    </a>
+          <a
+            href="https://github.com/kayro-svg"
+            className="footer_social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="uil uil-github-alt"></i>
+          </a>
 
-                    <a href="https://dribbble.com/KayroV" className="footer_social-link" target="_blank">
-                        <i class="uil uil-dribbble"></i>
-                    </a>
-                </div>
+          <a
+            href="https://dribbble.com/KayroV"
+            className="footer_social-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="uil uil-dribbble"></i>
+          </a>
+        </div>
 
-                <span className='footer_copy'>&#169; Kayro Valero - Portfolio. All rights reserved.</span>
-            </div>
-        </footer>
-    )
-}
+        <span className="footer_copy">
+          &#169; Kayro Valero - Portfolio. All rights reserved.
+        </span>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
 
+const WorkItems = ({ item }) => {
+  return (
+    <div className="work_card" key={item.id}>
+      <img src={item.image} alt="" className="work_img" />
+      <h3 className="work_title">{item.title}</h3>
+      <a
+        href={item.url}
+        target="_blank"
+        className="work_button"
+        rel="noreferrer"
+      >
+        View project <i className="bx bx-right-arrow-alt work_button-icon"></i>
+      </a>
+    </div>
+  );
+};
 
-const WorkItems = ({item}) => {
-    return (
-        <div className='work_card' key={item.id}>
-            <img src={item.image} alt='' className='work_img'/>
-            <h3 className="work_title">{item.title}</h3>
-            <a href={item.url} target='_blank' className="work_button">
-                View project <i className="bx bx-right-arrow-alt work_button-icon"></i>
-            </a>
-        </div>
-    ); 
-}
-
-export default WorkItems
+export default WorkItems;
