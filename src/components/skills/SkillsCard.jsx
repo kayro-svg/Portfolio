ecdf0677d036d49a61d5e065ca61e60f692ac3d4
Kayro Valero <sannelvalero@gmail.com>
2025-01-20 08:38:42 -0500

    Add SkillsCard component and refactor skills section

---
+++ src/components/skills/SkillsCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { competences } from "./competences";
+
+export default function SkillsCard() {
+  return (
+    <div className="skills_container container grid">
+      {competences.map((competence, index) => (
+        <div className="skills_content" key={index}>
+          <h3 className="skills_title">{competence.category}</h3>
+          <div className="skills_list">
+            <div className="skills_box">
+              <div className="skills_group">
+                {competence.skills.map((skill, index) => (
+                  <div className="skills_data" key={index}>
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                      <h3 className="skills_name">{skill.name}</h3>
+                      <span className="skills_level">{skill.level}</span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
