334207e68c19fc80b448dd9b9a40a0e3058837ff
Kayro Valero <sannelvalero@gmail.com>
2025-01-28 18:08:26 -0500

    refactor: Update project data and improve component structure for better clarity

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,63 +1,55 @@
-import Work1 from "../../assets/work1AutoParts.jpg"
-import Work2 from "../../assets/Work2AcademiaLEF.png"
-import Work3 from "../../assets/work3Walcakes.jpg"
-import Work4 from "../../assets/work4Fotoformas.jpg"
-import Work5 from "../../assets/work5Roxxet.jpg"
-import Work6 from "../../assets/Work6Esquematizate.png"
+import Work1 from "../../assets/work1EditorialEsquematizate.png";
+import Work2 from "../../assets/work2Santita.png";
+import Work3 from "../../assets/pledge4peace.png";
+import Work5 from "../../assets/work5Roxxet.jpg";
+import Work6 from "../../assets/Work6Esquematizate.png";
 
 export const projectsData = [
-    { 
-        id: 1,
-        image: Work1,
-        title: "Website eCommerce - AutoPartes",
-        category: 'design',
-        url: "https://zonasurautopartes.com/store/",
-    },
-    {
-        id: 2,
-        image: Work2,
-        title: "Website Development & SEO",
-        category: 'web',
-        url: "https://legionextranjera.es/",
-    },
-    {
-        id: 6,
-        image: Work6,
-        title: "Website Development & SEO",
-        category: 'web',
-        url: "https://esquematizate.com/",
-    },
-    {
-        id: 4,
-        image: Work3,
-        title: "Website Landing Page - Walcakes",
-        category: 'web',
-        url: "https://kayro-svg.github.io/Landing-Page-JS-PHP-HTML-CSS/",
-    },
-    {
-        id: 5,
-        image: Work4,
-        title: "Website Design - FotoFormas",
-        category: 'design',
-        url: "https://foto-formas.webflow.io/",
-    },
-    {
-        id: 3,
-        image: Work5,
-        title: "Website eCommerce - Roxxet",
-        category: 'web',
-        url: "https://roxett.com.mx/",
-    },
+  {
+    id: 1,
+    image: Work1,
+    title: "Website Development & SEO",
+    category: "web",
+    url: "https://editorial-esquematizate.es/",
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "Website improvements & UI optimization",
+    category: "web",
+    url: "https://santita.rnb.la/",
+  },
+  {
+    id: 3,
+    image: Work5,
+    title: "Website eCommerce - Roxxet",
+    category: "web",
+    url: "https://roxett.com.mx/",
+  },
+  {
+    id: 4,
+    image: Work3,
+    title: "Website Figma Redesign - Pledge4Peace",
+    category: "design",
+    url: "https://www.figma.com/design/w8WVANOwZcWbvlX9o6dLuE/Figma-Website-Design---Landing-Page?node-id=0-88&m=dev&t=c5RjzMbcamQfozAT-1",
+  },
+  {
+    id: 5,
+    image: Work6,
+    title: "Website Development & SEO",
+    category: "web",
+    url: "https://esquematizate.com/",
+  },
 ];
 
 export const projectsNav = [
-    {
-        name: 'all',
-    },
-    {
-        name: 'web',
-    },
-    {
-        name: 'design',
-    },
+  {
+    name: "all",
+  },
+  {
+    name: "web",
+  },
+  {
+    name: "design",
+  },
 ];
