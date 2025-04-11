9320373eec630fd46985a2d508119c87c8d4caad
Kayro Valero <sannelvalero@gmail.com>
2025-04-11 11:04:31 -0500

    feat: Add new project images and update projects data in Data.jsx

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -3,21 +3,17 @@ import Work2 from "../../assets/work2Santita.png";
 import Work3 from "../../assets/pledge4peace.png";
 import Work5 from "../../assets/work5Roxxet.jpg";
 import Work6 from "../../assets/Work6Esquematizate.png";
+import Work7 from "../../assets/srbrander.png";
+import Work8 from "../../assets/cieje.png";
+import Work9 from "../../assets/rnbapp.jpg";
 
 export const projectsData = [
   {
-    id: 1,
-    image: Work1,
-    title: "Website Development & SEO",
+    id: 6,
+    image: Work7,
+    title: "Website Redesign",
     category: "web",
-    url: "https://editorial-esquematizate.es/",
-  },
-  {
-    id: 2,
-    image: Work2,
-    title: "Website improvements & UI optimization",
-    category: "web",
-    url: "https://santita.rnb.la/",
+    url: "https://www.srbrander.com/",
   },
   {
     id: 3,
@@ -27,11 +23,39 @@ export const projectsData = [
     url: "https://roxett.com.mx/",
   },
   {
+    id: 7,
+    image: Work8,
+    title: "Website Design - CIEJE - On progress",
+    category: "design",
+    url: "https://kayro-portfolio.vercel.app/",
+  },
+  {
+    id: 8,
+    image: Work9,
+    title: "React Native App - RNB",
+    category: "all",
+    url: "https://apps.apple.com/pa/app/rednblue/id1643543709?l",
+  },
+  {
     id: 4,
     image: Work3,
     title: "Website Figma Redesign - Pledge4Peace",
     category: "design",
     url: "https://www.figma.com/design/w8WVANOwZcWbvlX9o6dLuE/Figma-Website-Design---Landing-Page?node-id=0-88&m=dev&t=c5RjzMbcamQfozAT-1",
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "Website improvements & UI optimization",
+    category: "web",
+    url: "https://santita.rnb.la/",
+  },
+  {
+    id: 1,
+    image: Work1,
+    title: "Website Development & SEO",
+    category: "web",
+    url: "https://editorial-esquematizate.es/",
   },
   {
     id: 5,
